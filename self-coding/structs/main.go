cadad3b1e75d89fed6d027d8da14e88fdda16111
phc-quang <quanggtangg@gmail.com>
2022-06-22 17:12:55 +0700

    learn: structs

---
+++ self-coding/structs/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Mee",
+		contactInfo: contactInfo{
+			email:   "email@gmail.com",
+			zipCode: 535243,
+		},
+	}
+
+	jim.updateName("quang")
+
+	jim.print()
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
