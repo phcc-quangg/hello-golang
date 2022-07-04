2e6f2550c89cd57598e963d7f34ab56eb974e5ab
phc-quangg <quang.tang@thuocsi.vn>
2022-07-04 13:13:32 +0700

    learn: completed

--- self-coding/channel/main.go
+++ self-coding/channel/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -13,18 +14,31 @@ func main() {
 		"http://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(c, link)
+	}
+
+	for l := range c {
+		go func(li string) {
+			time.Sleep(5 * time.Second)
+			checkLink(c, li)
+		}(l)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(c chan string, link string) {
+	time.Sleep(2 * time.Second)
+
 	_, err := http.Get(link)
 
 	if err != nil {
 		fmt.Println(link, " might be down!")
+		c <- link
 		return
 	}
 
 	fmt.Println(link, " is ok!")
+	c <- link
 }
